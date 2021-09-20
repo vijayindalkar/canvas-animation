97623cca99fe3dd1b98ee13caa07ff7fbc191d4e
vijayindalkar <61607592+vijayindalkar@users.noreply.github.com>
2021-09-20 21:43:43 +0530

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,19 +1,19 @@
-
-var widthoftable = 300;
-var heightoftable = 300;
+p = 10;
 
 
 var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 var c = canvas.getContext("2d");
 function drawBoard(){
-    for (var x = 0; x <= widthoftable; x += 100) {
-        c.moveTo(x + 10, 10);
-        c.lineTo(x + 10, heightoftable + 10);
+    for (var x = 0; x <= 300; x += 100) {
+        c.moveTo(x + p, p);
+        c.lineTo(x + p, 300 + p);
     }
     
-    for (var x = 0; x <= heightoftable; x += 100) {
-        c.moveTo(10, x + 10);
-        c.lineTo(widthoftable + 10, x + 10);
+    for (var x = 0; x <= 300; x += 100) {
+        c.moveTo(p, x + p);
+        c.lineTo(300 + p, x + 10);
     }
     c.strokeStyle = "black";
     c.stroke();
