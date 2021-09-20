62eea5ea77943aee89eed5044720ed5926b5c9d2
vijayindalkar <61607592+vijayindalkar@users.noreply.github.com>
2021-09-20 21:15:53 +0530

    changed code to grid creation

--- canvas.js
+++ canvas.js
@@ -1,63 +1,25 @@
+
+var widthoftable = 300;
+var heightoftable = 300;
+
+
 var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-var c = canvas.getContext('2d'); 
-c.fillStyle = 'white';
-c.fillRect(550,100,450,450); // fillrect(x,y,width,height)
-
-// line
-c.beginPath();
-c.moveTo(700,100);
-c.lineTo(700,550);
-// c.lineTo(70,100);
-c.strokeStyle = 'red';
-c.stroke();
-
-c.beginPath();
-c.moveTo(550,100);
-c.lineTo(550,550);
-c.strokeStyle = 'red';
-c.stroke();
-
-c.beginPath();
-c.moveTo(850,100);
-c.lineTo(850,550);
-c.strokeStyle = 'red';
-c.stroke();
-
-c.beginPath();
-c.moveTo(1000,100);
-c.lineTo(1000,550);
-c.strokeStyle = 'red';
-c.stroke();
-
-c.beginPath();
-c.moveTo(1000,250);
-c.lineTo(550,250);
-c.strokeStyle = 'red';
-c.stroke();
-
-c.beginPath();
-c.moveTo(1000,400);
-c.lineTo(550,400);
-c.strokeStyle = 'red';
-c.stroke();
-
-c.beginPath();
-c.moveTo(1000,550);
-c.lineTo(550,550);
-c.strokeStyle = 'red';
-c.stroke();
-
-c.beginPath();
-c.moveTo(1000,100);
-c.lineTo(550,100);
-c.strokeStyle = 'red';
-c.stroke();
-
+var c = canvas.getContext("2d");
+function drawBoard(){
+    for (var x = 0; x <= widthoftable; x += 100) {
+        c.moveTo(x + 10, 10);
+        c.lineTo(x + 10, heightoftable + 10);
+    }
+    
+    for (var x = 0; x <= heightoftable; x += 100) {
+        c.moveTo(10, x + 10);
+        c.lineTo(widthoftable + 10, x + 10);
+    }
+    c.strokeStyle = "black";
+    c.stroke();
+}
 
 canvas.addEventListener('mousemove',function(moved){
     console.log(moved);
 });
-
+drawBoard();    
