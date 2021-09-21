19a76fad0668a237af61cab3e1fa862bd2b691c1
vijayindalkar <61607592+vijayindalkar@users.noreply.github.com>
2021-09-22 02:09:34 +0530

    partially bug fixed

--- canvas.js
+++ canvas.js
@@ -1,25 +1,41 @@
-p = 10;
-
-
-var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
+const canvas = document.querySelector('canvas');
 canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
 var c = canvas.getContext("2d");
-function drawBoard(){
-    for (var x = 0; x <= 300; x += 100) {
-        c.moveTo(x + p, p);
-        c.lineTo(x + p, 300 + p);
-    }
-    
-    for (var x = 0; x <= 300; x += 100) {
-        c.moveTo(p, x + p);
-        c.lineTo(300 + p, x + 10);
-    }
-    c.strokeStyle = "black";
-    c.stroke();
+var x = null;
+let boxAreas = []
+
+function checkorientation(){
+	if(canvas.width > canvas.height){
+		x = canvas.height/3;
+	}
+	else{
+		x =canvas.width/3;
+	}
 }
 
-canvas.addEventListener('mousemove',function(moved){
-    console.log(moved);
-});
-drawBoard();    
+
+function drawBoard(){
+	checkorientation();
+	let maxBoxSide = x		
+	let xOffset = canvas.width/2 - maxBoxSide * 1.5
+	let yOffset = canvas.height/2 - maxBoxSide * 1.5
+	
+	for(let i=0; i<4; i++) {
+		c.moveTo(xOffset, yOffset + maxBoxSide * i);
+		c.lineTo(xOffset + maxBoxSide * 3, yOffset + maxBoxSide * i);
+		c.moveTo(xOffset + maxBoxSide * i, yOffset);    
+		c.lineTo(xOffset + maxBoxSide * i, yOffset + maxBoxSide * 3);
+	c.strokeStyle = "black";
+	c.stroke();
+	}
+}
+
+// window.addEventListener("resize", drawBoard);
+
+
+// canvas.addEventListener('mousemove',function(moved){
+// 		console.log(moved);
+// })
+drawBoard();
